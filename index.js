584420fca6b9b48916a6934c0727b79425e47e50
Jericho Santonia <jericho.santonia@vikingassetmanagement.com>
2023-11-29 20:43:34 +0800

    porfolio push

---
+++ index.js
@@ -0,0 +1,80 @@
+
+// Time 
+var today = new Date();
+
+var formattedTime = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+const timeNowDiv = document.getElementById("timeNow")
+
+timeNowDiv.innerHTML = formattedTime
+
+// Popup Form
+
+
+
+function toggleForm() {
+    var visibleDiv = document.getElementById("popupForm")
+    var overlayDiv = document.getElementById("overlay")
+
+    visibleDiv.classList.toggle('hidden');
+    overlayDiv.classList.toggle('hidden');
+    
+}
+
+function scrollToAbout() {
+    var targetSection = document.getElementById('aboutSection');
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToHome() {
+    var targetSection = document.getElementById('homeSection');
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToTool() {
+    var targetSection = document.getElementById('toolSection');
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToWork() {
+    var targetSection = document.getElementById('workSection');
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToWebDev() {
+    var targetSection = document.getElementById('webDevSection');
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToUIUX() {
+    var targetSection = document.getElementById('UiUxSection');
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAppDev() {
+    var targetSection = document.getElementById('appDevSection');
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function showImage(element) {
+    alert('sdas');
+    var modal = document.getElementById('imageModal');
+    var fullImage = document.getElementById('fullImage');
+    var previewImage = element.querySelector('img');
+  
+    modal.style.display = 'block';
+    fullImage.src = previewImage.src;
+
+
+  }
+  
+  function hideImage() {
+    document.getElementById('imageModal').style.display = 'none';
+  }
