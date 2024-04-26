4c02309c0b315db12ef5e5fc28a4787107ae7821
Jericho Santonia <jericho.santonia@vikingassetmanagement.com>
2024-04-26 14:54:56 +0800

    update responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
     
 
     screens: {
-      'sm': '640px', // Small devices (mobile)
+      'sm': '120px', // Small devices (mobile)
       'md': '768px', // Medium devices (tablets)
-      'lg': '1024px', // Large devices (desktops)
+      'lg': '1025px', // Large devices (desktops)
       'xl': '1280px', // Extra large devices (large desktops)
       '2xl': '1536px',
       '3xl': '1920px' // Extra extra large devices
@@ -15,8 +15,8 @@ module.exports = {
     extend: {
       spacing: {
         '22': '86px',
-        '110': '450px'
-        
+        '110': '450px',
+        '120': '570px',
       },
     },
   },
